059e9a4473a04909adfd9d8e60f42ac56ccea349
Karl Thoren <karl.h.thoren@gmail.com>
2017-01-10 12:24:04 +0100

    Removed GetCurrentFilename() handling as the filename is provided by the scanner buffer.

--- Asap2/Asap2.Scanner.cs
+++ Asap2/Asap2.Scanner.cs
@@ -11,24 +11,15 @@ namespace Asap2
         public override void yyerror(string format, params object[] args)
         {
             StringBuilder errorMsg = new StringBuilder();
-            errorMsg.AppendFormat("{0} : Line: {1} : Row: {2} : {3}", GetCurrentFilename(), yyline, yycol, string.Format(format, args));
+            errorMsg.AppendFormat("{0} : Line: {1} : Row: {2} : {3}", this.buffer.FileName, yyline, yycol, string.Format(format, args));
             errorHandler.reportError(errorMsg.ToString());
         }
 
         private IErrorReporter errorHandler;
-        private Stack<string> filenames = new Stack<string>();
-        public string GetCurrentFilename()
-        {
-            if (filenames.Count > 0)
-            {
-                return filenames.Peek();
-            }
-            return "";
-        }
+        private Stack<Stream> StreamStack = new Stack<Stream>();
 
-        public Asap2Scanner(Stream file, IErrorReporter errorHandler, string fileName) : this(file)
+        public Asap2Scanner(Stream file, IErrorReporter errorHandler) : this(file)
         {
-            this.filenames.Push(fileName);
             this.errorHandler = errorHandler;
         }
     }
