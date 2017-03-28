045f618a836f57e16b5ec3278b8f6a8d776a455b
Nathan Brown <nathan@nkbrown.us>
2017-03-27 18:57:04 -0700

    Add unit tests with API change approval.

---
+++ Asap2.Tests/ApiSpec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using PublicApiGenerator;
+
+namespace Asap2.Tests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class ApiSpec
+    {
+        [Test]
+        public void ApproveApiChanges()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(Asap2File).Assembly);
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
